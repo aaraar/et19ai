6ed752ca6df15d7adb220a25e835ead0cd8acea7
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-09-13 17:34:12 +0200

    attempt at spotify API

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Emerging tech AI assignment
 
 1. Run `npm install`
 2. Create a .env file in root
-3. Add API key from https://deepaffects.com/
-4. Run `npm start`
-5. go to localhost:3000/boos
+3. Add `DEEP_AFFECTS_API_KEY=y5yJgxCZvPKnPG9pT7sTSPBbBtoczTIa SPOTIFY_CLIENT_ID=<your spotify client id> SPOTIFY_CLIENT_SECRET=<your spotify client secret>`
+4. Add API key from https://deepaffects.com/
+5. Run `npm start`
+6. go to localhost:3000/boos
--- index.js
+++ index.js
@@ -1,27 +1,43 @@
 const DeepAffects = require("deep-affects");
 const defaultClient = DeepAffects.ApiClient.instance;
 const express = require("express");
+var cors = require("cors");
+var cookieParser = require("cookie-parser");
+
+var request = require("request");
 require("dotenv").config();
+const spotifyAuth = require("./spotifyAuthorization/userAuthorization");
+
 const port = 3000;
 const app = express();
 let emoData;
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-// Configure API key authorization: UserSecurity
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+app
+	.use(express.static(__dirname + "/public"))
+	.use(cors())
+	.use(cookieParser());
+
 const UserSecurity = defaultClient.authentications["UserSecurity"];
-UserSecurity.apiKey = process.env.API_KEY;
+UserSecurity.apiKey = process.env.DEEP_AFFECTS_API_KEY;
 // Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
 //UserSecurity.apiKeyPrefix = 'Token';
 
 const apiInstance = new DeepAffects.EmotionApi();
-
 const body = DeepAffects.Audio.fromFile("./boos.wav"); // {Audio} Audio object
-const callback = function(error, data, response) {
+
+apiInstance.syncRecogniseEmotion(body, (error, data, response) => {
 	if (error) {
 		console.error(error);
 	} else {
 		console.log("API called successfully. Returned data: " + response);
 		emoData = JSON.parse(response.text);
 	}
-};
-apiInstance.syncRecogniseEmotion(body, callback);
-app.get("/boos", (req, res) => res.send(emoData));
+});
+
+app
+	.get("/test", (req, res) => res.send(body))
+	.get("/boos", (req, res) => res.send(emoData))
+	.get("/login", spotifyAuth.onLogin)
+	.get("/callback", spotifyAuth.onCallback)
+	.get("/refresh_token", spotifyAuth.onRefreshToken);
