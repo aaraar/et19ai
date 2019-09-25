4cce5fc6154b6b0a7842efe968ff4dbb1a8a51e0
Bas de Greeuw <bas.de.greeuw@van-ons.nl>
2019-09-25 13:18:34 +0200

    spotify test

--- spotifyAuthorization/userAuthorization.js
+++ spotifyAuthorization/userAuthorization.js
@@ -13,7 +13,7 @@ require("dotenv").config();
 
 const client_id = process.env.SPOTIFY_CLIENT_ID; // Your client id
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET; // Your secret
-const redirect_uri = "https://voicemood.herokuapp.com/public/none.html"; // Your redirect uri
+const redirect_uri = "https://voicemood.herokuapp.com/static/none.html"; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
